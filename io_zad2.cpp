3bb882300a7916ca3957c3b6979578820c0c4f6a
white67 <52746153+white67@users.noreply.github.com>
2023-10-25 16:23:49 +0200

    Add files via upload

---
+++ io_zad2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // zadanie 2
+
+    int tablica[10];
+    int* tab = new int[10];
+    int ilosc = 0;
+
+    for (int i = 0; i < 10; i++) {
+        cout << "\nWprowadz liczbe nr " << i+1 << ": ";
+        cin >> tablica[i];
+        if (tablica[i] < 0) {
+            tab[ilosc] = tablica[i];
+            ilosc++;
+        }
+    }
+    cout << "\nWprowadzono " << ilosc << " liczb ujemnych";
+    cout << "\nPominieto " << 10-ilosc << " liczb";
+    cout << "\nOto wszystkie liczby ujemne: ";
+    for (int i = 0; i < ilosc; i++) {
+        cout << "\n" << tab[i];
+    }
+
+    delete[] tab;
+}
---
+++ io_zad4.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+struct Produkt {
+    string nazwa;
+    int cena;
+};
+
+void bubbleSort(Produkt arr[], int n)
+{
+    int i, j;
+    bool swapped;
+    for (i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j].cena > arr[j + 1].cena) {
+                swap(arr[j].cena, arr[j + 1].cena);
+                swapped = true;
+            }
+        }
+        if (swapped == false)
+            break;
+    }
+}
+
+int main()
+{
+
+    // zadanie 4
+
+    Produkt lista[10];
+
+    for (int i = 0; i < 10; i++) {
+        string nazwa;
+        int cena;
+        cout << "\nWprowadz produkt nr " << i + 1 << ".";
+        cout << "\nNazwa: ";
+        cin >> nazwa;
+        cout << "Cena: ";
+        cin >> cena;
+        lista[i].nazwa = nazwa;
+        lista[i].cena = cena;
+    }
+
+    Produkt lista2[10];
+
+    bubbleSort(lista, 10);
+
+    for (int i = 9; i >= 0; i--) {
+        cout << "\n" << lista[i].nazwa << " - " << lista[i].cena;
+    }
+
+}
