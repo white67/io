63dadae2912a7770fb13e38c44104dc09b567ae2
white67 <52746153+white67@users.noreply.github.com>
2023-11-08 16:25:42 +0100

    Add files via upload

---
+++ diagram-aktywnosci.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+class Currencies {
+public:
+    float usd = 4.15;
+    float eur = 4.44;
+    float gbp = 5.10;
+};
+
+class User {
+    float balanace;
+};
+
+int main()
+{
+    
+}
