78230e4665a5245f370500dc1345cf597ec8b7e6
white67 <52746153+white67@users.noreply.github.com>
2023-11-14 23:25:16 +0100

    Update diagram-aktywnosci.cpp

--- diagram-aktywnosci.cpp
+++ diagram-aktywnosci.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 
 class Currencies {
@@ -6,13 +6,35 @@ public:
     float usd = 4.15;
     float eur = 4.44;
     float gbp = 5.10;
+    float calcCurrency(string currency, float money) {
+        if (currency == "usd")
+            return money / usd;
+        else if (currency == "eur")
+            return money / eur;
+        else if (currency == "gbp")
+            return money / gbp;
+        else
+            return -1;
+    }
 };
 
 class User {
-    float balanace;
+public:
+    float balance;
 };
 
 int main()
 {
-    
+    User uzytkownik;
+    Currencies baza_walut;
+    uzytkownik.balance = 105.45;
+
+    cout << "Saldo uzytkownika: " << uzytkownik.balance << " pln";
+    cout << endl;
+    cout << baza_walut.calcCurrency("usd", uzytkownik.balance) << " usd";
+    cout << endl;
+    cout << baza_walut.calcCurrency("eur", uzytkownik.balance) << " eur";
+    cout << endl;
+    cout << baza_walut.calcCurrency("gbp", uzytkownik.balance) << " gbp";
+    cout << endl;
 }
